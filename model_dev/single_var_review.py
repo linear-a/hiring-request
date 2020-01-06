b4a196271a52b1ef25716b17db80a0537b34a109
bnebeker <brett.nebeker@outlook.com>
2020-01-05 19:44:24 -0700

    REF: update report load, check wrvus

--- model_dev/single_var_review.py
+++ model_dev/single_var_review.py
@@ -3,17 +3,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 pd.set_option('max_columns', 500)
 
-df = pd.read_csv(
+df_report = pd.read_csv(
     './model_dev/dev_assets/outputs/single_variable_report.csv.gz',
     compression='gzip'
 )
 
-print(df.head())
+print(df_report.head())
 
-df.sort_values(by='lm_auc', ascending=False).head(20)
+df_report.sort_values(by='lm_auc', ascending=False).head(20)
 
 # TEST CORRELATION
 _df = pd.read_csv('./model_dev/dev_assets/data/1_clean_fe.csv.gz', compression='gzip')
+print(_df.groupby('icce')['target'].sum())
+print(_df[_df.icce == 'Cancer']['target'])
 
 # STRONGEST LINEAR: provider_clinical_fte_sum_mav_5_vs_total TO target_60
 target = 'target_30'
@@ -40,3 +42,7 @@ df_t1[feature].hist()
 
 print(df_t0[feature].describe())
 print(df_t1[feature].describe())
+
+
+wrvu = df_report[df_report.feature.str.contains('wrv')]
+wrvu.sort_values(by='lm_auc', ascending=False).head(20)
